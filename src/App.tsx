e0aeff7f42a68551600677941f68bba571fffb53
Argi <argiigmg@gmail.com>
2026-05-08 22:16:25 +0700

    Implement structure for code changes with placeholders for future updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet-async";
+
 import About from "./pages/About";
 import Experience from "./pages/Experiences";
 import Hero from "./pages/Hero";
@@ -7,14 +9,55 @@ import SkillsPage from "./pages/Skills";
 
 function App() {
   return (
-    <section className="container max-w-3xl mx-auto p-6">
-      <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <ProjectsPage />
-      <SkillsPage />
-    </section>
+    <>
+      <Helmet>
+        <title>Argi - Fullstack Developer</title>
+        <meta
+          name="description"
+          content="Hallo saya Argi Ahmes Halepiyandra, fresh graduate SMK jurusan Rekayasa Perangkat Lunak."
+        />
+        <meta
+          name="keywords"
+          content="argiiahm, argi, fullstack developer, react developer, laravel developer, portfolio, jasa pembuatan website, elunive, elunivedev, jasa pembuatan website cilacap"
+        />
+        <meta name="author" content="Argiahm" />
+        <meta name="robots" content="index, follow" />
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://elunivedev.my.id" />
+        {/* Open Graph */}
+        <meta property="og:title" content="Argi - Fullstack Developer" />
+        <meta
+          property="og:description"
+          content="Hallo saya Argi Ahmes Halepiyandra, fresh graduate SMK jurusan Rekayasa Perangkat Lunak."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://elunivedev.my.id" />
+        <meta
+          property="og:image"
+          content="https://elunivedev.my.id/preview.png"
+        />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Argi - Fullstack Developer" />
+        <meta
+          name="twitter:description"
+          content="Hallo saya Argi Ahmes Halepiyandra, fresh graduate SMK jurusan Rekayasa Perangkat Lunak."
+        />
+        <meta
+          name="twitter:image"
+          content="https://elunivedev.my.id/preview.png"
+        />
+      </Helmet>
+
+      <section className="container max-w-3xl mx-auto p-6">
+        <Navbar />
+        <Hero />
+        <About />
+        <Experience />
+        <ProjectsPage />
+        <SkillsPage />
+      </section>
+    </>
   );
 }
 
